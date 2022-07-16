ff88e481128f41c18af68cf028851e8ac0e25e16
swapnilvj <svjagadhane21@gmail.com>
2022-07-16 20:08:02 +0530

    RestTemplate code refactored
    - RestTemplate refactored to handle all the SupportedMediaTypes.
    - Exception Handling logic refactored to handle HttpClientErrorException and HttpServerErrorException.
    - Extracted logical code snippets to form  small and meaningful methods.
    - Constants class created to maintain all the constants.

---
+++ src/main/java/com/smaato/RestServiceConstants.java
@@ -0,0 +1,9 @@
+package com.smaato;
+
+public class RestServiceConstants {
+    public static final String ENDPOINT_RESPONSE_STATUS = "Endpoint Response Status: %s";
+    public static final String QUERY_PARAM_ENDPOINT = "countOfUniqueIds";
+    public static final String REST_SERVICE_EXCEPTION_ERROR_MSG = "RestService Exception for id : %d and endpoint : %s";
+    public static final String APPLICATION_COULD_NOT_PROCESS_THE_RESPONSE_ERROR_MSG = "Application Could not process the response for id: %s and endpoint: %s";
+
+}
--- src/main/java/com/smaato/RestServiceImpl.java
+++ src/main/java/com/smaato/RestServiceImpl.java
@@ -1,61 +1,69 @@
 package com.smaato;
 
-import com.sun.deploy.net.HttpResponse;
-import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.beans.Beans;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static com.smaato.RestServiceConstants.*;
 
 @Service
 public class RestServiceImpl implements RestService {
 
     @Override
     public void getCall(String id, String endpoint) throws RestServiceException {
-        String urlTemplate = UriComponentsBuilder.fromHttpUrl(endpoint)
-                .queryParam("endpoint", "{countOfUniqueIds}")
-                .encode()
-                .toUriString();
+        String endpointUrlTemplate = buildEndpointUrlTemplate(endpoint);
 
-        Map<String, Integer> params = new HashMap<>();
-        //TODO Get count
-        params.put("countOfUniqueIds", 10);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
+        Map<String, Integer> params = buildQueryParamMap();
 
         try {
-            RestTemplate restTemplate = new RestTemplate();
+            RestTemplate restTemplate = buildRestTemplate();
 
-            HttpEntity<Object> response = restTemplate.exchange(
-                    urlTemplate,
-                    HttpMethod.GET,
-                    entity,
-                    Object.class,
-                    params
-            );
+            ResponseEntity<Object> responseEntity = restTemplate.getForEntity(endpointUrlTemplate, Object.class, params);
             //TODO Log HttpStatus
-            
-        } catch (RestClientException restClientException) {
-            //TODO Log error
-            System.out.println(restClientException.getMessage());
-            //TODO Throw application exception
-            throw new RestServiceException("Invalid endpoint provided.");
+            System.out.println(String.format(ENDPOINT_RESPONSE_STATUS,responseEntity.getStatusCode()));
+
+
+        } catch (HttpClientErrorException | HttpServerErrorException httpClientOrServerExc) {
+            //TODO Log
+            System.out.println(String.format(ENDPOINT_RESPONSE_STATUS,httpClientOrServerExc.getStatusCode()));
+            throw new RestServiceException(String.format(REST_SERVICE_EXCEPTION_ERROR_MSG, id, endpoint));
         } catch (Exception ex) {
             //TODO Log error
             System.out.println(ex.getMessage());
+            System.out.println(String.format(APPLICATION_COULD_NOT_PROCESS_THE_RESPONSE_ERROR_MSG, id, endpoint));
             throw ex;
         }
+    }
+
+    private Map<String, Integer> buildQueryParamMap() {
+        Map<String, Integer> params = new HashMap<>();
+        //TODO Get count
+        params.put("count", 10);
+        return params;
+    }
+
+    private String buildEndpointUrlTemplate(String endpoint) {
+        return UriComponentsBuilder.fromHttpUrl(endpoint)
+                    .queryParam(QUERY_PARAM_ENDPOINT, "{count}")
+                    .encode()
+                    .toUriString();
+    }
+
+    private RestTemplate buildRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+        messageConverters.add(converter);
+        restTemplate.setMessageConverters(messageConverters);
+        return restTemplate;
     }
 }
